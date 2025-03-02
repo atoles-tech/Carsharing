dda93bda5e34a01fa4339edd7087ec31ca673f68
Atoles <evgenijkhodosok@gmail.com>
2025-03-02 15:43:50 +0300

    i understanded how i must to create a project

---
+++ admin-module/src/main/java/carsharing/admin/App.java
@@ -0,0 +1,13 @@
+package carsharing.admin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ server-module/src/main/java/carsharing/client/server/App.java
@@ -0,0 +1,9 @@
+package carsharing.client.server;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    }
+}
